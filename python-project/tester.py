c523fe3db4fe342d22103a8ccd0af46e51f2f806
CI - SERAUM <ci@seraum.com>
2017-08-07 17:32:01 +0200

    CI

--- python-project/tester.py
+++ python-project/tester.py
@@ -4,11 +4,6 @@ needed = open ("test_universe.py", "r")
 correct = output.readline()
 user = needed.readline()
 
-print ("file.txt = ")
-print (correct)
-print ("test universe =")
-print (user)
-
 if correct != user:
     exit(1)
 else:
