135ba2af46598471264052eccf2081ed892609c3
CI - SERAUM <ci@seraum.com>
2017-08-18 10:28:02 +0200

    CI

--- algo_exo/test.py
+++ algo_exo/test.py
@@ -3,7 +3,7 @@ from subprocess import *
 commande = ["gcc", "for_user.c"]
 out = Popen(commande,stdout=PIPE)
 (sout,serr) = out.communicate()
-print sout
+print sout;
 
 _exec = Popen(["./a.out"], stdout=PIPE);
 (eOut, eErr) = _exec.communicate();
