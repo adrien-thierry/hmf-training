7e2bb9680e899331d5d98eff5202e9ddbbbf0581
CI - SERAUM <ci@seraum.com>
2017-08-18 12:54:02 +0200

    CI

--- sql_exo/tester_sql.py
+++ sql_exo/tester_sql.py
@@ -1,7 +1,7 @@
 output = open("for_user_sql.txt", "r")
 needed = open ("correct_sql.txt", "r")
 
-correct = output.readline()
+correct = output.readline().split("#")[0]
 user = needed.readline()
 
 if correct != user:
