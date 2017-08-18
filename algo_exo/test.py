25a0c34b8760459d68a03ee5c9c7d0cfe25a78ee
CI - SERAUM <ci@seraum.com>
2017-08-18 10:46:01 +0200

    CI

--- algo_exo/test.py
+++ algo_exo/test.py
@@ -3,11 +3,9 @@ from subprocess import *
 commande = ["gcc", "for_user.c"];
 out = Popen(commande,stdout=PIPE);
 (sout,serr) = out.communicate();
-print (sout);
 
 _exec = Popen(["./a.out"], stdout=PIPE);
 (eOut, eErr) = _exec.communicate();
-print (eOut);
 
 needed = open ("correct.txt", "r");
 user = needed.readline();
