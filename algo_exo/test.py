7e451eb26e378117da4d1fb440bce6aa9557cbea
CI - SERAUM <ci@seraum.com>
2017-08-18 11:05:02 +0200

    CI

--- algo_exo/test.py
+++ algo_exo/test.py
@@ -10,8 +10,6 @@ _exec = Popen(["./a.out"], stdout=PIPE);
 needed = open ("correct.txt", "r");
 user = needed.readline();
 
-print (eOut)
-print (user)
 if eOut != user:
     print("TECHIO> success false");
 else:
