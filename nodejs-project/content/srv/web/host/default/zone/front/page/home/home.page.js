cc8dc6bf1808739570ddda509dee07b84e3934d6
root <root@matrice.matrice.local>
2017-09-11 12:22:13 +0200

    salut

--- nodejs-project/content/srv/web/host/default/zone/front/page/home/home.page.js
+++ nodejs-project/content/srv/web/host/default/zone/front/page/home/home.page.js
@@ -6,7 +6,13 @@ function homePage(ok)
 {
     this.code = function(req, res)
     {
+	var uri = req.url.split("?");
+	if(uri[1]) uri = uri[1]
+	else uri = "";
         if(req.get.id && req.get.id =="'") console.log("TECHIO> success true");
-        res.end(this.view.home);
+        
+	var view = this.view.home.toString();
+	view = view.replace("$_URL", uri);
+	res.end(view);
     };
 }
