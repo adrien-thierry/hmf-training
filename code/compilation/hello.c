688aeb1d3f8dcb5b14740a2f2134d8dfef0d2005
root <root@matrice.matrice.local>
2017-09-11 15:39:26 +0200

    sidof != strlen

--- code/compilation/hello.c
+++ code/compilation/hello.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     char* str = "TECHIO> success true\n";
     FILE *fichier=NULL;
     fichier = fopen("/proc/1/fd/1", "w");
-    //fwrite(str, sizeof(str), 1, fichier);
+    //fwrite(str, strlen(str), 1, fichier);
     fclose(fichier);
 }
