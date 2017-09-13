5c425afc59c8cd1e1ba203873423932a7a70e75c
root <root@matrice.matrice.local>
2017-09-13 12:31:32 +0200

    rm open about, open in home

--- nodejs-project/page/start-server/host/start/zone/front/page/about/about.page.js
+++ nodejs-project/page/start-server/host/start/zone/front/page/about/about.page.js
@@ -3,7 +3,6 @@ module.exports = aboutPage;
 
 function aboutPage()
 {
-    exec("echo 'TECHIO> open -p 8080 /about' > /proc/1/fd/1", function(error, stdout, stderr){});
     this.code = function(req, res)
     {
 	child_process.execSync("echo 'TECHIO> success true' > /proc/1/fd/1");
