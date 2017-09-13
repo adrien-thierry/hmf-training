1397de07b4b339fa12d06e092dc7122bd7d27332
root <root@matrice.matrice.local>
2017-09-13 11:52:07 +0200

    wrong path for exo 3

--- nodejs-project/arch/arch.sh
+++ nodejs-project/arch/arch.sh
@@ -8,13 +8,4 @@ git config --global http.sslVerify false
 
 echo 'TECHIO> open -p 8080 /home'
 
-while :
-do
-  if [ ! -f "/tmp/end.txt" ]; then
-  sleep 1s
-  else
-     cat "/tmp/end.txt"
-     sleep 3s
-     exit 0
-  fi
-done
+sleep 5m
