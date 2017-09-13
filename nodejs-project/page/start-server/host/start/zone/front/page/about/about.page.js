070be151a9dee9574fd13b2eacce2bef02f70614
root <root@matrice.matrice.local>
2017-09-13 12:26:34 +0200

    s

--- nodejs-project/page/start-server/host/start/zone/front/page/about/about.page.js
+++ nodejs-project/page/start-server/host/start/zone/front/page/about/about.page.js
@@ -3,7 +3,7 @@ module.exports = aboutPage;
 
 function aboutPage()
 {
-    exec("echo 'TECHIO> open -p 8080 /home' > /proc/1/fd/1", function(error, stdout, stderr){});
+    exec("echo 'TECHIO> open -p 8080 /about' > /proc/1/fd/1", function(error, stdout, stderr){});
     this.code = function(req, res)
     {
 	child_process.execSync("echo 'TECHIO> success true' > /proc/1/fd/1");
