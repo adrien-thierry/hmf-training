2be0269869c5e08b27806702b55a4248b2d5435d
root <root@matrice.matrice.local>
2017-09-13 11:26:30 +0200

    gi

--- nodejs-project/code/arch/arch.sh
+++ nodejs-project/code/arch/arch.sh
@@ -6,6 +6,14 @@ apt-get install -y git nodejs
 
 git config --global http.sslVerify false
 
+git clone https://github.com/seraum/fortressjs
+
+cp -r start-engine fortressjs/content/engine/
+
+cp -r start-server fortressjs/content/srv/
+
+cd fortressjs && nodejs wf.js
+
 echo 'TECHIO> terminal'
 
 while :
